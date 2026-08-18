81ba427a9ffc3444613bbc0d081551587c39d852
Jean Piero <jeanpiero@gmail.com>
2026-08-17 20:53:02 -0500

    fix: update Supabase credentials to new project

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://rkxvkhwdmuqxnbdniqvl.supabase.co'
-const SUPABASE_ANON = 'sb_publishable_I3fkp6sY_bXR524O1kMLxw_4J-DHMj4'
+const SUPABASE_URL = 'https://jxjyxaadzcyrclxhazwi.supabase.co'
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4anl4YWFkemN5cmNseGhhendpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMDc0NDYsImV4cCI6MjEwMjU4MzQ0Nn0.hXJSlHwqQwN_GU6im4NZIwk6d1ZnMGrH5SrpvWCiSbU'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON)
